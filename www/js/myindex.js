dd7998db0fdba2da39d103f3a6ecee1c0aab443a
xmpxraw <xmpxraw@163.com>
2015-12-11 14:03:53 +0800

    增加demo页面

---
+++ www/js/myindex.js
@@ -0,0 +1,43 @@
+define([], function() {
+  return View.extend({
+    events: {
+
+    },
+    render: function() {    
+        this.startSwiper();
+        this.startContentSwiper();
+    },
+
+    startSwiper: function(){
+            if($.Swiper){//初始化swiper
+                var _this = this;
+                var $el = _this.$el;
+                var myslider = new $.Swiper($el.find('.swiper-container')[0],{
+                    speed: 400,
+                    spaceBetween: 0,
+                    loop: true,
+                    pagination : '.swiper-pagination',
+                    paginationClickable : true,
+                    autoplay : 3000,
+                    autoplayDisableOnInteraction : false,
+                })
+            }  
+    },
+
+    startContentSwiper: function(){
+            if($.Swiper){//初始化swiper
+                var _this = this;
+                var $el = _this.$el;
+                var myslider = new $.Swiper($el.find('.content-container')[0],{
+                    spaceBetween: 0,
+                    loop: true,
+                    pagination : '.radius',
+                    paginationClickable : true,
+                    autoplayDisableOnInteraction : false,
+                })
+            }  
+    },
+
+
+  });
+});
