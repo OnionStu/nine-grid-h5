6be9546f8c4481157d877a6caa2ca2d103aa30b2
xmpxraw <xmpxraw@163.com>
2015-12-15 11:35:38 +0800

    增加更新按钮

--- www/js/myindex.js
+++ www/js/myindex.js
@@ -2,7 +2,7 @@ define([], function() {
   return View.extend({
     events: {
         'click .appDown' : 'showDownMask',
-        'click .dontNeed' : 'hideDownMask',
+        'click .footRight' : 'abc'
     },
     render: function() {    
         this.startSwiper();
@@ -10,19 +10,23 @@ define([], function() {
     },
 
     startSwiper: function(){
-            if($.Swiper){//初始化swiper
-                var _this = this;
-                var $el = _this.$el;
-                var myslider = new $.Swiper($el.find('.swiper-container')[0],{
-                    speed: 400,
-                    spaceBetween: 0,
-                    loop: true,
-                    pagination : '.swiper-pagination',
-                    paginationClickable : true,
-                    autoplay : 3000,
-                    autoplayDisableOnInteraction : false,
-                })
-            }  
+        if($.Swiper){//初始化swiper
+            var _this = this;
+            var $el = _this.$el;
+            var myslider = new $.Swiper($el.find('.swiper-container')[0],{
+                speed: 400,
+                spaceBetween: 0,
+                loop: true,
+                pagination : '.swiper-pagination',
+                paginationClickable : true,
+                autoplay : 3000,
+                autoplayDisableOnInteraction : false,
+            })
+        }  
+    },
+
+    console: function(){
+        console.log("click");
     },
 
     startContentSwiper: function(){
@@ -43,12 +47,8 @@ define([], function() {
         $('.maskLayer').removeClass("hidden");
     },
 
-
-    hideDownMask: function(){
-        alert("message");
+    abc: function(){
         $('.maskLayer').addClass("hidden");
-    },
-
-
+    }
   });
 });
